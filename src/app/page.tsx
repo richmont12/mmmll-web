8536029d80ba825393b3d27de617e981652273fc
Richard Heininger <rhe@indeca.de>
2023-11-05 12:18:28 +0100

    feat: adapted home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+
+/*
+
+
+          Wähle zwischen
+          Blitzwortlesen (Wort wird für ms sekunden angezeigt)
+            - Anzeigezeit eingebbar
+            - Angabe pro Wort ob es gelesen werden konnte
+            - Wiederholungen möglich
+            - mit auswertung
+          Text lesen
+            - Zeit messen anfang bis ende
+            - mit pause option
+            - textgröße einstellbar
+            - mit auswertung
+
+          Design
+            - hintergrund nicht ganz weiß
+            - wenig text
+            - schwarze schrift
+            - dark mode für mich?
+
+
+*/
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.headline}>
+        <b>Willkommen bei &quot;Mach Mir Mein Leben leichter&quot;</b>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.function}>
+          <button>Blitzwort lesen</button>
+        </div>
+        <div className={styles.function}>
+          <button>Text lesen</button>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <div className={styles.footer}>
+        <div className={styles.footerHeadline}>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Implementiert von{" "}
+            <a href="https://github.com/richmont12">richmont</a> mit Hilfe von
           </p>
-        </a>
+        </div>
+        <div className={styles.footerImages}>
+          <Image
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={90}
+            height={18}
+            priority
+          />
+          <Image
+            src="/mui.svg"
+            alt="Material UI Logo"
+            width={90}
+            height={18}
+            priority
+          />
+        </div>
       </div>
     </main>
-  )
+  );
 }
